31d5ac4c526101caf7de73966e8cd612ef1b155a
Viktoria <zaharova181114@gmail.com>
2021-09-10 09:24:57 +0300

    media

--- app/js/common.js
+++ app/js/common.js
@@ -18,5 +18,25 @@ $('.category-section-slider').slick({
     // variableWidth: true,
     infinite: false,
     prevArrow: '<button type="button" class="slick-prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"></button>'
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
